9faad5bd0bffbb60f3d831dd5ffa772674b6c047
Jacob Arriola <jacob@zeek.com>
2017-07-01 08:48:05 -0700

    ⬆️ Update foundation-sites 6.4.0 -> 6.4.1
    Addresses MediaQuery JS bug for the addToJquery() method

--- assets/js/foundation.js
+++ assets/js/foundation.js
@@ -1,10 +1,7 @@
 import { Foundation } from 'foundation-sites/js/foundation.core';
 import { Reveal } from 'foundation-sites/js/foundation.reveal';
-import { MediaQuery } from 'foundation-sites/js/foundation.util.mediaQuery';
 
 Foundation.plugin(Reveal, 'Reveal');
 
-// MediaQuery is oddly needed for the addToJqueryMethod
-Foundation.MediaQuery = MediaQuery;
 Foundation.addToJquery(jQuery);
 jQuery(document).foundation();
